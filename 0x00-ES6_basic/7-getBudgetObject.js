70202e40dc02d7c3569fee8ac45556f56b8730a3
aya-cs22 <aya786930@gmail.com>
2024-07-23 01:20:32 +0300

    Object property value shorthand syntax

---
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable */
+export default function getBudgetObject(income, gdp, capita) {
+    const budget = {
+      inc: income,
+      gp: gdp,
+      cap: capita,
+    };
+  
+    return budget;
+  }
+  
