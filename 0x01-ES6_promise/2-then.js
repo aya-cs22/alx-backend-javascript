5590cb15cf06a94b718e1fb70556e5f21e454820
aya-cs22 <aya786930@gmail.com>
2024-07-24 15:30:31 +0300

    Handle multiple successful promises

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable */
+export default function handleResponseFromAPI(promise) {
+    return new Promise((resolve, reject) => {
+      promise
+        .then((response) => {
+          resolve(response);
+        })
+        .catch((error) => {
+          reject(new Error(''));
+        })
+        .finally(() => {
+          console.log('Got a response from the API');
+        });
+    });
+  }
+  
