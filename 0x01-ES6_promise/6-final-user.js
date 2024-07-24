27e7631ee099095fb30cfb4138363956d5aa3be5
aya-cs22 <aya786930@gmail.com>
2024-07-24 19:49:08 +0300

    Handle multiple promises

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,18 @@
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return new Promise((resolve, reject) => {
+    if (firstName && lastName && fileName) {
+        Promise.all([
+        signUpUser(firstName, lastName),
+        uploadPhoto(fileName),
+      ]);
+    }
+  }).then((response) => {
+    return response.map(response({
+      status: response.status,
+      value: response.status === 'fulfilled' ? response.value : response.reason,
+    }));
+  });
+}
