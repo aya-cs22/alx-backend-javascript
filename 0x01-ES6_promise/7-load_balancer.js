c2cfcd1c22089ba561fccc7076984a4e5a728fce
aya-cs22 <aya786930@gmail.com>
2024-07-24 20:15:30 +0300

    Load balancer

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,13 +1,3 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable */
-export default loadBalancer((chinaDownload, USDownload) =>{
-  return new Promise((resolve, reject) => {
-    if (chinaDownload && USDownload){
-        resolve([chinaDownload, USDownload]);
-    } else {
-      reject(new Error("Both downloads must be provided."));
-    }
-  }) .then((response) => {
-    return Promise.race(response)
-  })
-});
+export default function loadBalancer(chinaDownload, USDownload) {
+  return Promise.race([chinaDownload, USDownload]);
+  }
