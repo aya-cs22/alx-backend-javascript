c36787a15e995409f6d6862dd1827be4e4431f6e
aya-cs22 <aya786930@gmail.com>
2024-07-24 18:54:13 +0300

    Reject the promises

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,7 +1,10 @@
 export default function uploadPhoto(filename) {
-  return new Promise((resolve, reject) => {
-    if (filename){
-        reject(new Error(`$fileName cannot be processed`));
-    }
-  })
-}
+    return new Promise((resolve, reject) => {
+      if (filename) {
+        reject(new Error(`${filename} cannot be processed`));
+      } else {
+        reject(new Error('Filename is required'));
+      }
+    });
+  }
+  
