7e49a1bf30dbba10a780f38b700b83c99b487432
aya-cs22 <aya786930@gmail.com>
2024-07-24 16:35:39 +0300

    Handle multiple successful promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable */
-import { uploadPhoto, createUser } from './utils.js'; // Corrected import statement
+import { uploadPhoto, createUser } from './utils.js';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
