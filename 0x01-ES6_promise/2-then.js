24c8863462923f6ffd7d37b6af2998859e3a1114
aya-cs22 <aya786930@gmail.com>
2024-07-24 15:41:07 +0300

    Catch me if you can!

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,16 +1,14 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable */
 export default function handleResponseFromAPI(promise) {
-    return new Promise((resolve, reject) => {
-      promise
-        .then((response) => {
-          resolve({ status: 200, body: 'Success' });
-        })
-        .catch((error) => {
-          reject(new Error(''));
-        })
-        .finally(() => {
-          console.log('Got a response from the API');
-        });
+return pomise
+    .then((response) => {
+        resolve({status: 200, body: 'Success'});
+    })
+    .catch((error) => {
+        reject(new Error(''));
+    })
+    .finally(() => {
+        console.log('Got a response from the API');
     });
-  }
+    }
