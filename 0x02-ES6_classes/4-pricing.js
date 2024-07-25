81528818e204abf5dd3b7056e4c11650489ea39d
aya-cs22 <aya786930@gmail.com>
2024-07-25 23:11:57 +0300

    Pricing

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,34 @@
+import Currency from './3-currency';
+
+export default class extends Currency {
+  constructor(amount, currency) {
+    super(currency.name, currency.code);
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set amount(value) {
+    this._amount = value;
+  }
+
+  set currency(value) {
+    this._currency = value;
+  }
+
+  displayFullPrice() {
+    const currency = this._currency;
+    return `${this.amount} ${currency.name} ${currency.code}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
