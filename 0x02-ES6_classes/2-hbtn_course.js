41f4d70e60de936df35a4c435e8247223dd0d1c4
aya-cs22 <aya786930@gmail.com>
2024-07-25 14:48:37 +0300

    A Course, Getters, and Setters

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -19,21 +19,21 @@ export default class HolbertonCourse {
 
   set name(value) {
     if (typeof value !== 'string') {
-      throw new Error('Name must be a string');
+      throw new TypeError('Name must be a string');
     }
     this._name = value;
   }
 
   set length(value) {
     if (typeof value !== 'number') {
-      throw new Error('Length must be a number');
+      throw new TypeError('Length must be a number');
     }
     this._length = value;
   }
 
   set students(value) {
     if (!Array.isArray(value)) {
-      throw new Error('Students must be an array');
+      throw new TypeError('Students must be an array');
     }
     this._students = value;
   }
