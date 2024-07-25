f64deb02b2f06fffe61a78f9b8daf2f7e3004c32
aya-cs22 <aya786930@gmail.com>
2024-07-25 20:26:52 +0300

    Methods, static methods, computed methods names..... MONEY

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,26 @@
+export default class ClassRoom {
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set name(value) {
+    this._name = value;
+  }
+
+  set code(value) {
+    this._code = value;
+  }
+
+  get displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
+}
