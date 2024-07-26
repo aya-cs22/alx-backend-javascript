04843ac7c12e287d239d68c8d727d6137fa98c6c
aya-cs22 <aya786930@gmail.com>
2024-07-26 12:22:46 +0300

    For...of Loops

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-    for (let idx of array) {
-      let value = array[idx];
-      array[idx] = appendString + value;
-    }
-  
-    return array;
+  const newArray = [];
+  for (const idx of array) {
+    newArray.push(appendString + idx);
   }
+
+  return newArray;
+}
