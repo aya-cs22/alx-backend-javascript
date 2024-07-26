c83e6be36ecd3b2987906fe524cce2309d259850
aya-cs22 <aya786930@gmail.com>
2024-07-26 12:01:24 +0300

    Handle multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,12 +1,36 @@
+// Import the necessary modules
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
+// Define and export the handleProfileSignup function
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  // Use Promise.allSettled to wait for both promises to settle
+  const responses = await Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]).then((results) => results.map(({ status, value, reason }) => ({
-    status,
-    value: status === 'fulfilled' ? value : reason,
-  })));
+  ]);
+
+  // Map over the responses to transform them into the desired structure
+  const transformedResponses = responses.map(response => ({
+    status: response.status,
+    value: response.status === 'fulfilled' ? response.value : response.reason,
+  }));
+
+  // Return the transformed responses
+  return transformedResponses;
 }
+
+// import signUpUser from './4-user-promise';
+// import uploadPhoto from './5-photo-reject';
+
+// export default async function handleProfileSignup(firstName, lastName, fileName) {
+//   return Promise.allSettled([
+//     signUpUser(firstName, lastName),
+//     uploadPhoto(fileName),
+//   ]).then((response) => {
+//     return response.map(response({
+//       status: response.status,
+//       value: response.status === 'fulfilled' ? response.value : response.reason,
+//     }));
+//   });
+// }
