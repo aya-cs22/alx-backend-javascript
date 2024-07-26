e68229b2a9db903aac89015664837237ed533b11
aya-cs22 <aya786930@gmail.com>
2024-07-26 19:05:07 +0300

    Handle multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,27 +1,15 @@
+
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise
-    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((res) => (
-      res.map((o) => ({
-        status: o.status,
-        value: o.status === 'fulfilled' ? o.value : String(o.reason),
-      }))
-    ));
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((responses) => (
+    responses.map((response) => ({
+      status: response.status,
+      value: response.status === 'fulfilled' ? response.value : response.reason,
+    }))
+  ));
 }
-// import signUpUser from './4-user-promise';
-// import uploadPhoto from './5-photo-reject';
-
-// export default function handleProfileSignup(firstName, lastName, fileName) {
-//   return Promise.allSettled([
-//     signUpUser(firstName, lastName),
-//     uploadPhoto(fileName),
-//   ]).then((responses) => {
-//     responses.map((response) => ({
-//       status: response.status,
-//       value: response.status === 'fulfilled' ? response.value : response.reason,
-//     }));
-//   });
-// }
