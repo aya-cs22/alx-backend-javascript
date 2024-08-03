1c112bb28a5ba1f0936d36c8e70ee19196c091fc
aya-cs22 <aya786930@gmail.com>
2024-08-03 11:17:44 +0300

    task_2

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -61,3 +61,11 @@ function executeWork(employee: Director | Teacher): void {
         else
             console.log(employee.workTeacherTasks());
     }
+
+type Subjects = "Math" | "History";
+function teachClass(todayClass: Subjects): string{
+    if(todayClass === "Math")
+        return "Teaching Math";
+    else if (todayClass === "History")
+        return "Teaching History";
+}
