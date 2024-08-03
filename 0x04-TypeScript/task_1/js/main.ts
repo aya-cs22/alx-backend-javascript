e671bbeeb4158b51f011cf9d9577750f722dd60e
aya-cs22 <aya786930@gmail.com>
2024-08-03 09:39:41 +0300

    task_1

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -29,3 +29,9 @@ const director1: Directors = {
     numberOfReports: 17,
   };
   console.log(director1);
+
+function printTeacher(firstName:string , lastName: string) : string{
+    return `${firstName.charAt(0)}. ${lastName}`;
+}
+
+console.log(printTeacher("John", "Doe"));
