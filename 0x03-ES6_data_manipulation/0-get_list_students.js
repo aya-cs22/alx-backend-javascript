e3063dcf4f0aafc02ac64c1b7b38c6859c4bd15a
aya-cs22 <aya786930@gmail.com>
2024-08-04 12:25:46 +0300

    Basic list of objects

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,7 @@
 export default function getListStudents() {
   return[
-    {firstName :"Guillaume", id: 1, location: "Francisco"},
-    {firstName :"James", id: 2, location: "Columbia"},
-    {firstName :"Serena", id: 5, location: "Francisco"},
+    {id: 1, firstName :"Guillaume", location: "Francisco"},
+    {id: 2, firstName :"James", location: "Columbia"},
+    {id: 5, firstName :"Serena", location: "Francisco"},
     ]
 }
