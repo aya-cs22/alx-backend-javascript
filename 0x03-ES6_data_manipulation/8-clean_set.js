c0bc67ff44cf7208100d6846463d9d54f26b1c3a
aya-cs22 <aya786930@gmail.com>
2024-08-04 17:26:26 +0300

    Clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,15 @@
 export default function cleanSet(set, startString) {
   const result = new Set();
-  if (startString === '' && typeof startString !== 'string') {
+  if (startString === '') {
     return '';
   }
+
+  for (const value of set) {
+    if (typeof value !== 'string') {
+      return ''; // Return empty string if any non-string type is found
+    }
+  }
+
   for (const value of set) {
     if (typeof value === 'string' && value.startsWith(startString)) {
       result.add(value.slice(startString.length));
