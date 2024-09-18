1b8692f8b04c19ca21f57ec0939a6105556b965f
aya-cs22 <aya786930@gmail.com>
2024-09-18 08:26:40 +0300

    Create a more complex HTTP server using Express

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,17 +1,41 @@
-const express = require('express');
+// const express = require('express');
 
-const port = 1245;
+// const port = 1245;
+// const app = express();
+// app.get('/', (req, res) => {
+//   res.send('Hello Holberton School!');
+// });
+
+// aapp.get('/students', async (req, res) => {
+//     res.type('text/plain');
+//     const studentsList = await getStudentsList(path.join(__dirname, 'database.csv'));
+//     res.send(`This is the list of our students\n${studentsList}`);
+//   });
+// app.listen(port, () => {
+// });
+
+// module.exports = app;
+#!/usr/bin/node
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
 const app = express();
+const port = 1245;
+
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
-aapp.get('/students', async (req, res) => {
-    res.type('text/plain');
-    const studentsList = await getStudentsList(path.join(__dirname, 'database.csv'));
-    res.send(`This is the list of our students\n${studentsList}`);
-  });
-app.listen(port, () => {
+app.get('/students', (req, res) => {
+  countStudents(process.argv[2])
+    .then((result) => {
+      res.send(`This is the list of our students\n${result.join('\n')}`);
+    })
+    .catch((error) => {
+      res.status(500).send(`This is the list of our students\n${error.message}`);
+    });
 });
+
+app.listen(port);
 
 module.exports = app;
