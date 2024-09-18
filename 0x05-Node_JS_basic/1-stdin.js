47699f5e87745ee0c4e89f6e3b0e4f9b25704170
aya-cs22 <aya786930@gmail.com>
2024-09-18 05:11:01 +0300

    Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,4 +2,9 @@ console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
   console.log(`Your name is: ${name}`);
+  console.log('This important software is now closing');
+});
+process.on('SIGINT', () => {
+  console.log('\nThis important software is now closing');
+  process.exit();
 });
