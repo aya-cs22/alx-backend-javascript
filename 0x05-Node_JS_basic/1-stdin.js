ec5ef205c3168718e00f2ea36260f6949c7aadc1
aya-cs22 <aya786930@gmail.com>
2024-09-18 05:20:35 +0300

    Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,9 +1,13 @@
-console.log('Welcome to Holberton School, what is your name?');
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  console.log(`Your name is: ${name}`);
-  console.log('This important software is now closing');
-});
-process.on('SIGINT', () => {
+// 1-stdin.js
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('data', (input) => {
+  const name = input.toString().trim();
+  process.stdout.write(`Your name is: ${name}\n`);
+  // Check if the input is coming from a pipe (not interactive input)
+  if (!process.stdin.isTTY) {
+    process.stdout.write('This important software is now closing\n');
+  }
+
   process.exit();
 });
