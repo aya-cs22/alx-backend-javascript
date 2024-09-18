5906eeb59a7c57a7f67e0dd0bb6c7aa37fbc6f5a
aya-cs22 <aya786930@gmail.com>
2024-09-18 08:28:08 +0300

    Create a more complex HTTP server using Express

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,20 +1,3 @@
-// const express = require('express');
-
-// const port = 1245;
-// const app = express();
-// app.get('/', (req, res) => {
-//   res.send('Hello Holberton School!');
-// });
-
-// aapp.get('/students', async (req, res) => {
-//     res.type('text/plain');
-//     const studentsList = await getStudentsList(path.join(__dirname, 'database.csv'));
-//     res.send(`This is the list of our students\n${studentsList}`);
-//   });
-// app.listen(port, () => {
-// });
-
-// module.exports = app;
 #!/usr/bin/node
 const express = require('express');
 const countStudents = require('./3-read_file_async');
