05c765a01b2632ea9070a28ef34c5021bd8acfde
aya-cs22 <aya786930@gmail.com>
2024-09-18 07:48:49 +0300

    Create a small HTTP server using Nodes HTTP module

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,8 +1,7 @@
 const http = require('http');
 const hostMan = 'localhost';
 const port = 1254;
-const app = http.createServer();
-app.on((req, res) => {
+const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!');
