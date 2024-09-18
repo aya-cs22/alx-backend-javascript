798dc05e98419603d9d243b9a5f98b095d59e8e2
aya-cs22 <aya786930@gmail.com>
2024-09-18 10:47:39 +0300

    Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,26 +1,9 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
-
-if (process.stdin.isTTY) {
-  process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data.toString()}`);
-    process.exit();
-  });
-} else {
-  process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data.toString()}`);
-    process.exit();
-  });
-  process.on('exit', () => {
+process.stdin.on('data', (input) => {
+  const name = input.toString().trim();
+  process.stdout.write(`Your name is: ${name}\n`);
+  if (!process.stdin.isTTY) {
     process.stdout.write('This important software is now closing\n');
-  });
-}
-// process.stdout.write('Welcome to Holberton School, what is your name?\n');
-
-// process.stdin.on('data', () => {
-//   const name = input.toString().trim();
-//   process.stdout.write(`Your name is: ${name}\n`);
-//   if (!process.stdin.isTTY) {
-//     process.stdout.write('This important software is now closing\n');
-//   }
-//   process.exit();
-// });
+  }
+  process.exit();
+});
