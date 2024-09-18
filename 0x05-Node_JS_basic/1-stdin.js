01863489c2760b70c5ee70bb24a894cbdd5ab533
aya-cs22 <aya786930@gmail.com>
2024-09-18 05:00:45 +0300

    Using Process stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,5 @@
+console.log('Welcome to Holberton School, what is your name?')
+process.stdin.on('data', (data) => {
+  const name = data.toString().trim();
+  console.log(`Your name is: ${name}`);
+});
