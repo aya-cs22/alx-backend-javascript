a4af9beeb884a062710fce3c877a49b0285c08be
aya-cs22 <aya786930@gmail.com>
2024-09-18 07:01:56 +0300

    Reading a file asynchronously with Node JS

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+
+const countStudents = async (path) => {
+  try {
+    const data = await fs.readFileSync(path, 'utf-8');
+    //     data.split('\n')
+    // [
+    //     "firstname,lastname,age,field",
+    //     "Johann,Kerbrou,30,CS",
+    //     "Guillaume,Salou,30,SWE",
+    //     "Arielle,Salou,20,CS"
+    //   ]
+    const lines = data.split('\n').filter(line => line.trim() !== '');
+
+    if (lines.length <= 1) {
+      throw new Error('Cannot load the database');
+    }
+
+    // const headers = lines[0].split(',');
+    // ["firstname", "lastname", "age", "field"]
+    const students = lines.slice(1);
+    console.log(`Number of students: ${students.length}`);
+
+    const fieldCounts = {};
+
+    students.forEach((student) => {
+      const studentData = student.split(',');
+      const field = studentData[3];
+      const firstName = studentData[0];
+
+      if (!fieldCounts[field]) {
+        fieldCounts[field] = [];
+      }
+
+      fieldCounts[field].push(firstName);
+    });
+
+    for (const [field, names] of Object.entries(fieldCounts)) {
+      console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
+    }
+  } catch (err) {
+    console.error('Cannot load the database');
+  }
+};
+module.exports = countStudents;
