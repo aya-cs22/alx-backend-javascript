b678555eda5668748a6d84ec347239d8a9adddae
aya-cs22 <aya786930@gmail.com>
2024-09-18 08:05:05 +0300

    5. Create a more complex HTTP server using Nodes HTTP module

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const countStudents = require('./2-read_file');
+
+const hostMan = 'localhost';
+const port = 1254;
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  if (req.url === '/') {
+    res.write('Hello Holberton School!');
+    res.end('');
+  }
+  if (req.url === '/students') {
+    const result = countStudents(process.argv[2]);
+    res.write(result);
+  } catch (error) {
+    res.write('Cannot load the database');
+  }
+  res.end();
+});
+
+app.listen(port, hostMan, () => {
+
+});
+
+module.exports = app;
