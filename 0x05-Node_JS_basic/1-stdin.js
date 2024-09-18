d2fe3e1e16368fb388e1e8c25c884a4ff81e27cd
aya-cs22 <aya786930@gmail.com>
2024-09-18 10:44:49 +0300

    Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,18 +1,22 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('readable', () => {
-  const chunk = process.stdin.read();
-
-  if (chunk) {
-    process.stdout.write(`Your name is: ${chunk}`);
-  }
-});
-
-process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
-});
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
 // process.stdout.write('Welcome to Holberton School, what is your name?\n');
-// process.stdin.on('data', (input) => {
+
+// process.stdin.on('data', () => {
 //   const name = input.toString().trim();
 //   process.stdout.write(`Your name is: ${name}\n`);
 //   if (!process.stdin.isTTY) {
