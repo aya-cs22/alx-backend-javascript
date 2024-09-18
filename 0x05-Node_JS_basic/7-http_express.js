c2377e6abff156821435eb44753449bb0d660465
aya-cs22 <aya786930@gmail.com>
2024-09-18 20:10:54 +0300

    tasks

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,17 +1,37 @@
 const express = require('express');
+const countStudents = require('./3-read_file_async');
 
-const port = 1245;
 const app = express();
+const PORT = 1245;
+
 app.get('/', (req, res) => {
-  res.send('Hello Holberton School!');
+  res.end('Hello Holberton School!');
 });
 
-aapp.get('/students', async (req, res) => {
-    res.type('text/plain');
-    const studentsList = await getStudentsList(path.join(__dirname, 'database.csv'));
-    res.send(`This is the list of our students\n${studentsList}`);
-  });
-app.listen(port, () => {
+app.get('/students', (req, res) => {
+  const initialHeaderResponse = 'This is the list of our students\n';
+  countStudents(process.argv[2])
+    .then((data) => res.end(`${initialHeaderResponse}${data.join('\n')}`))
+    .catch((err) => res.end(`${initialHeaderResponse}${err.message}`));
 });
+
+app.listen(PORT);
 
 module.exports = app;
+// const express = require('express');
+
+// const port = 1245;
+// const app = express();
+// app.get('/', (req, res) => {
+//   res.send('Hello Holberton School!');
+// });
+
+// aapp.get('/students', async (req, res) => {
+//     res.type('text/plain');
+//     const studentsList = await getStudentsList(path.join(__dirname, 'database.csv'));
+//     res.send(`This is the list of our students\n${studentsList}`);
+//   });
+// app.listen(port, () => {
+// });
+
+// module.exports = app;
