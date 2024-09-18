6ea4641a1c6f04e1e97365272e941ba96c0ea0e0
aya-cs22 <aya786930@gmail.com>
2024-09-18 11:49:18 +0300

    task

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,21 @@
+// process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+// process.stdin.on('data', (input) => {
+//   const name = input.toString().trim();
+//   process.stdout.write(`Your name is: ${name}\n`);
+
+//   if (!process.stdin.isTTY) {
+//     process.stdout.write('This important software is now closing\n');
+//   }
+
+//   process.exit();
+// });
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', (input) => {
-  const name = input.toString().trim();
-  process.stdout.write(`Your name is: ${name}\n`);
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data}`);
+});
 
-  if (!process.stdin.isTTY) {
-    process.stdout.write('This important software is now closing\n');
-  }
-
-  process.exit();
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
