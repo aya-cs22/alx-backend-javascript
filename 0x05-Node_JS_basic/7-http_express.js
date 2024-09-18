9b682cac44dc1631f18bddaa4b88bb7e2ca13da8
aya-cs22 <aya786930@gmail.com>
2024-09-18 12:09:41 +0300

    task

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,24 +1,17 @@
-#!/usr/bin/node
 const express = require('express');
-const countStudents = require('./3-read_file_async');
 
-const app = express();
 const port = 1245;
-
+const app = express();
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
-app.get('/students', (req, res) => {
-  countStudents(process.argv[2])
-    .then((result) => {
-      res.send(`This is the list of our students\n${result.join('\n')}`);
-    })
-    .catch((error) => {
-      res.status(500).send(`This is the list of our students\n${error.message}`);
-    });
+aapp.get('/students', async (req, res) => {
+    res.type('text/plain');
+    const studentsList = await getStudentsList(path.join(__dirname, 'database.csv'));
+    res.send(`This is the list of our students\n${studentsList}`);
+  });
+app.listen(port, () => {
 });
-
-app.listen(port);
 
 module.exports = app;
